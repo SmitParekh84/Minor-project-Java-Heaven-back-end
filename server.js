1cffb7e080d12a8c786637b822ff2fde335516b5
preetpatel9341 <preetpatel290803@gmail.com>
2024-10-25 14:48:16 +0530

    cors enabled

--- server.js
+++ server.js
@@ -25,14 +25,17 @@ app.use(helmet());
 // Middleware for logging
 app.use(morgan("combined"));
 
+
+app.use(cors()); // Allow all origins temporarily (for debugging)
+
 // Middleware to enable CORS
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL, // Allow requests from your frontend
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-    credentials: true, // Enable credentials if needed
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL, // Allow requests from your frontend
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+//     credentials: true, // Enable credentials if needed
+//   })
+// );
 
 app.use((req, res, next) => {
   console.log('Request Origin:', req.headers.origin);
